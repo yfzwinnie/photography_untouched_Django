007e20318f7b82799a8aa67ecac91d54c9c70fb8
Winnie Zhang <yfz.winnie@gmail.com>
2017-07-21 15:13:54 -0700

    Initial commit

---
+++ photography_untouched/urls.py
@@ -0,0 +1,16 @@
+from django.conf import settings
+from django.conf.urls.static import static
+from django.conf.urls import url
+from django.contrib import admin
+from photos.views import index, about, contact, trees
+
+urlpatterns = [
+    url(r'^admin/', admin.site.urls),
+    url(r'^$', index, name='index'),
+    url(r'^about/', about, name='about'),
+    url(r'^contact/', contact, name='contact'),
+    url(r'^trees/', trees, name='trees')
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
