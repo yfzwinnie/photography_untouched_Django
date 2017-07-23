3717c758924bccb0275623775b1e50d033952854
Winnie Zhang <yfz.winnie@gmail.com>
2017-07-23 00:14:42 -0700

    edit setting for deployment

--- photography_untouched/settings.py
+++ photography_untouched/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '283wjpxs7$eyq$p*2$djbw6w-ufe^_aznwavacnn*j2v*)*0ye'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1' , 'photographyuntouched.herokuapp.com']
 
